a1cf0149b52b4afd8aa1cd222b10b65efed99a57
kyosuke uryu <kyo.uryu33@gmail.com>
2019-12-22 17:03:09 +0900

    [add]taskとuserのアソシエーション実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,7 +3,8 @@ class Task < ApplicationRecord
   validates :name, presence: true
   validates :name, length: { maximum: 30 }
   validate :validate_name_not_including_comma
-  
+  belongs_to :user
+
   private
 
   def validate_name_not_including_comma
