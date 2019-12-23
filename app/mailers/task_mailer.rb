c64a3d054341d96ef839bb72f07fe59a430d0168
kyosuke uryu <kyo.uryu33@gmail.com>
2019-12-23 19:46:10 +0900

    [add]メイラー実装

--- app/mailers/task_mailer.rb
+++ app/mailers/task_mailer.rb
@@ -2,8 +2,8 @@ class TaskMailer < ApplicationMailer
   def creation_email(task)
     @task = task
     mail(
-      subject: 'タスク作成完了メール'
-      to: 'user@example.com'
+      subject: 'タスク作成完了メール',
+      to: 'user@example.com',
       from: 'taskleaf@example.com'
     )
   end
